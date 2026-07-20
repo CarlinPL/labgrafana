aa2a60cb2d918c90baf180c36f6b8c3b3fdee61d
Carlos <coc2017cce@gmail.com>
2026-07-20 01:34:05 -0300

    fix ec2.tf and elastic_ip.tf

--- terraform/elastic_ip.tf
+++ terraform/elastic_ip.tf
@@ -1,12 +1,21 @@
-resource "aws_eip" "ip_pub" {
+resource "aws_eip" "zabbix_server_eip" {
+    instance = aws_instance.zabbix_server.id
     domain = "vpc"
+
     tags = {
-        Name = "meu-ip-publico"
+        Name = "eip-zabbix-server"
+        role = "zabbix_server"
     }
 }
 
-resource "aws_eip_association" "assoc_ip" {
-    instance_id = aws_instance.zabbix_server.id 
-    allocation_id = aws_eip.ip_pub.id
+resource "aws_eip" "hosts_eip" {
+    count = var.instance_count
+    instance = aws_instance.hosts[count.index].id
+    domain = "vpc"
+
+    tags = {
+      Name = "eip-monitoring-host-${count.index}"
+      role = "zabbix_agent"
+    }
 }
 
