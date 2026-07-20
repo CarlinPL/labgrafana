8162aa26ab8648c07b2dce38f1d99464f1d704b4
Carlos <coc2017cce@gmail.com>
2026-07-20 01:11:50 -0300

    fix outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "public_ip" {
-  value = aws_instance.monitoring.public_ip
+  value = aws_instance.zabbix_server.public_ip
 }
 
 output "grafana_url" {
-  value       = "http://${aws_instance.monitoring.public_ip}:3000"
+  value       = "http://${aws_instance.zabbix_server.public_ip}:3000"
 }
 
 output "zabbix_url" {
-  value       = "http://${aws_instance.monitoring.public_ip}:8080"
+  value       = "http://${aws_instance.zabbix_server.public_ip}:8080"
 }
